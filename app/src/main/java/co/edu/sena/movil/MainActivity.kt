5ab3cfc5d45145fd6373a32ec412ba1007f407c5
Michelballen13 <159820747+Michelballen13@users.noreply.github.com>
2024-07-22 22:05:33 -0500

    Update MainActivity.kt

--- app/src/main/java/co/edu/sena/movil/MainActivity.kt
+++ app/src/main/java/co/edu/sena/movil/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MovilTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "michel ballen",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
